8acff73d5c1262f4348dd040c85f0ab8777dd4f1
Nathalia Macedo <nathscalercio@gmail.com>
2026-02-10 12:09:42 -0300

    02_model_repository - model e repository

---
+++ src/model/Produto.ts
@@ -0,0 +1,42 @@
+import { colors } from "../util/Colors_temp";
+import { formatarMoeda } from "../util/Currency";
+
+export abstract class Produto {
+  constructor(
+    private _id: number,
+    private _titulo: string,
+    private _preco: number
+  ) {}
+
+  public get id(): number {
+    return this._id;
+  }
+  public set id(value: number) {
+    this._id = value;
+  }
+
+  public get titulo(): string {
+    return this._titulo;
+  } 
+  public set titulo(value: string) {
+    this._titulo = value;
+  }
+
+  public get preco(): number {
+    return this._preco;
+  }
+  public set preco(value: number) {
+    this._preco = value;
+  }
+
+  public visualizar(): void {
+    console.log(colors.fg.magenta,"\n**********************", colors.reset);
+    console.log("Dados do Livro")
+    console.log(`ID: ${this.id}`);
+    console.log(`Título: ${this.titulo}`);
+    console.log(`Preço: R$ ${this.preco.toFixed(2)}`);
+    console.log(colors.fg.magenta,"\n**********************", colors.reset);
+}
+}
+
+
